5253f008d0c0d9fb70b75db88e5fe051b3db5893
im-bipo <bipinkhatri.ram@gmail.com>
2025-02-04 19:16:00 +0545

    get certificate with certificate id

--- src/api/certificate/controllers/certificate.ts
+++ src/api/certificate/controllers/certificate.ts
@@ -1,7 +1,30 @@
-/**
- * certificate controller
- */
+import { factories } from "@strapi/strapi";
 
-import { factories } from '@strapi/strapi'
+export default factories.createCoreController(
+  "api::certificate.certificate",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const certificateID = ctx.params.id;
+      const populate = ctx.query.populate || {};
+      try {
+        const result = await strapi.entityService.findMany(
+          "api::certificate.certificate",
+          {
+            filters: { certificateID },
+            limit: 1,
+            populate,
+          }
+        );
 
-export default factories.createCoreController('api::certificate.certificate');
+        if (!result || result.length === 0) {
+          return ctx.notFound("Certificate not found");
+        }
+
+        // Maintain standard response format
+        return this.transformResponse(result[0]);
+      } catch (err) {
+        ctx.internalServerError("Something went wrong");
+      }
+    },
+  })
+);
